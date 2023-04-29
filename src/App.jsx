65605a0612535b0df30c9fe848f5803a79240fee
Fabricioga17 <fabricioruben19942@gmail.com>
2023-04-29 18:05:15 -0300

    empezando home

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import { Error } from "./page/Error";
 import { Login } from "./page/Login";
 import { Register } from "./page/Register";
 import { Contact } from "./page/Contact";
+import  'bootstrap/dist/css/bootstrap.min.css' ;
+
+
 
 function App() {
 
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,9 +1,53 @@
 import { Layout } from "../Layouts/Layout"
+import { Carousel, CarouselItem} from "react-bootstrap"
 
 export const Home = () => {
   return (
     <Layout>
     <h1>Home</h1>
+
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://media.istockphoto.com/id/500466008/es/foto/bistecs-de-carne-de-res.jpg?s=612x612&w=0&k=20&c=sVc8KhnTkebIWmwTeNYmXqGfXikut1iclMQ8K8NxZv8="
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://definicion.de/wp-content/uploads/2009/06/gastronomia.jpg"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://img.freepik.com/foto-gratis/chef-colocando-hierba-comida-gourmet_23-2148516908.jpg"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+
+
     </Layout>
   )
 }
